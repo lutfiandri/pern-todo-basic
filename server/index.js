eccaa7adfdf7172961111cd0030e4240f027c628
lutfiandri <lutfiandri232@gmail.com>
2021-01-07 22:38:43 +0700

    fix minor

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,4 +13,4 @@ const todoRoutes = require('./routes/todos');
 
 app.use('/api/todos/', todoRoutes);
 
-app.listen(PORT, () => `server running on port ${PORT}...`);
+app.listen(PORT, () => console.log(`server running on port ${PORT}...`));
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -3,11 +3,12 @@ const pool = require('../db');
 
 /**
  * TODO ROUTES
- * 1. get    '/'        -> get all todos
- * 2. get    '/:id'     -> get single todo
- * 3. post   '/'        -> add a todo
- * 4. put    '/:id'     -> update a todo
- * 5. delete '/:id'     -> delete a todo
+ * 1. get    '/'          -> get all todos
+ * 2. get    '/:id'       -> get single todo
+ * 3. post   '/'          -> add a todo
+ * 4. put    '/:id'       -> update a todo
+ * 5. put    '/:id/status -> toggle status
+ * 6. delete '/:id'       -> delete a todo
  */
 
 const router = express.Router();
@@ -81,28 +82,58 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const { title, description, status } = req.body;
+    const { title, description } = req.body;
     const todo = await pool.query(
       `
       UPDATE todos
       SET
         title = $1,
-        description = $2,
-        status = $3
-      WHERE id = $4
+        description = $2
+      WHERE id = $3
     `,
-      [title, description, status, id]
+      [title, description, id]
     );
     if (todo.rowCount === 0) {
-      res.status(400).json({
+      res.status(404).json({
         error: {
-          status: 400,
-          message: 'failed to update',
+          message: `there's no todo with id ${id}`,
         },
       });
     } else {
       res.json({
         message: 'todo updated',
+      });
+    }
+  } catch (err) {
+    res.status(400).json({
+      error: {
+        message: err.message,
+      },
+    });
+  }
+});
+
+router.put('/:id/status', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+    const todo = await pool.query(
+      `
+      UPDATE todos
+      SET status = $1
+      WHERE id = $2
+    `,
+      [status, id]
+    );
+    if (todo.rowCount === 0) {
+      res.status(404).json({
+        error: {
+          message: `there's no todo with id ${id}`,
+        },
+      });
+    } else {
+      res.json({
+        message: `todo updated.. id: ${id}, status: ${status}`,
       });
     }
   } catch (err) {
